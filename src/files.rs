4ac2f8ddc8c8b152247d007f354675625a0c5f8e
Jevo1900 <jevo1900@gmail.com>
2020-11-13 20:01:45 +0100

    first server try

--- src/files.rs
+++ src/files.rs
@@ -1,31 +1,61 @@
 use std::path::Path;
 use std::fs;
 use std::io::Read;
+use std::slice::Iter;
 
-pub fn file() {
+pub struct FileData {
+    file_name: String,
+    file_path: String,
+}
+
+impl FileData {
+    fn new(name: String, path: String) -> FileData {
+        return FileData { file_name: name, file_path: path };
+    }
+
+    pub fn name(&self) -> &String {
+        return &self.file_name;
+    }
+
+    pub fn path(&self) -> &String {
+        return &self.file_path;
+    }
+}
+
+pub struct DirFiles {
+    files: Vec<FileData>,
+}
+
+impl DirFiles {
+    fn new() -> DirFiles {
+        return DirFiles { files: Vec::new() };
+    }
+
+    fn push(&mut self, file: FileData) {
+        self.files.push(file);
+    }
+
+    fn list() {}
+
+    pub fn iter(&self) -> Iter<'_, FileData> {
+        return self.files.iter();
+    }
+}
+
+pub fn file(base_folder: String) -> DirFiles {
+    let mut files = DirFiles::new();
+
     // Create a path to Serve Directory
-    let path: &Path = Path::new("./public_http");
+    let path: &Path = Path::new(&base_folder);
 
     if path.exists() && path.is_dir() {
         let paths = fs::read_dir(path).unwrap();
         for route in paths {
-            println!("{}",route.unwrap().path().display());
+            let entry = route.unwrap();
+            files.push(FileData::new(entry.file_name().as_os_str().to_str().unwrap().to_string(),
+                                     entry.path().to_str().unwrap().to_string()));
         }
     }
-
-    if path.is_file() {
-        let display = path.display();
-        // Open the path in read-only mode, returns `io::Result<File>`
-        let mut file = match fs::File::open(&path) {
-            Err(why) => panic!("couldn't open {}: {}", display, why),
-            Ok(file) => file,
-        };
-// Read the file contents into a string, returns `io::Result<usize>`
-        let mut s = String::new();
-        match file.read_to_string(&mut s) {
-            Err(why) => panic!("couldn't read {}: {}", display, why),
-            Ok(_) => print!("{} contains:\n{}", display, s),
-        }
-    }
+    return files;
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,28 @@
 mod files;
 
-use std::convert::Infallible;
+use warp::Filter;
 use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server};
-use hyper::service::{make_service_fn, service_fn};
+use files::file;
 
 #[tokio::main]
 async fn main() {
-    // We'll bind to 127.0.0.1:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let base_folder = "public_http";
 
-    // A `Service` is needed for every connection, so this
-    // creates one from our `hello_world` function.
-    let make_svc = make_service_fn(|_conn| async {
-        // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(hello_world))
-    });
-
-    let server = Server::bind(&addr).serve(make_svc);
-
-    // Run this server for... forever!
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
+    let files = file(base_folder.to_string());
+    for file in files.iter() {
+        println!("File {{\n    \"name\":\"{}\"\n    \"path\":\"{}\"\n}}",file.name(),file.path());
     }
-}
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello, World".into()))
+    pretty_env_logger::init();
+    let _readme = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::file("./README.md"));
+
+    let _host = warp::header::<SocketAddr>("host");
+
+    // GET / => README.md
+    // GET /ex/... => ./examples/..
+    let routes = warp::fs::dir(base_folder).or();
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
