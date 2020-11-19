d7aed42ad9db12ef26ff7fd48f2ada6bff40b05b
Jevo1900 <jevo1900@gmail.com>
2020-11-19 20:37:41 +0100

    added template support and lcores usage calculation

---
+++ src/get.rs
@@ -0,0 +1,28 @@
+use rocket_contrib::templates::Template;
+use rocket::response::Redirect;
+use crate::TemplateContext;
+
+#[get("/")]
+pub fn index() -> Redirect {
+    Redirect::to("/hello/Unknown")
+}
+
+#[get("/hello/<name>")]
+pub fn hello(name: String) -> Template {
+    Template::render("index", &TemplateContext {
+        title: "Hello",
+        name: Some(name),
+        items: vec!["One", "Two", "Three"],
+        parent: "layout",
+    })
+}
+
+#[get("/about")]
+pub fn about() -> Template {
+    Template::render("about", &TemplateContext {
+        title: "About",
+        name: None,
+        items: vec!["Four", "Five", "Six"],
+        parent: "layout",
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,66 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+/// Rust packages for http requests
+mod get;
+mod post;
+mod put;
+mod delete;
 
-use rocket::Config;
+#[macro_use]
+extern crate rocket;
+extern crate num_cpus;
+
+use rocket::{Request, Config};
+use rocket_contrib::templates::{Template, handlebars};
+
+#[derive(serde::Serialize)]
+struct TemplateContext {
+    title: &'static str,
+    name: Option<String>,
+    items: Vec<&'static str>,
+    // This key tells handlebars which template is the parent.
+    parent: &'static str,
+}
+
+#[catch(404)]
+fn not_found(req: &Request<'_>) -> Template {
+    let mut map = std::collections::HashMap::new();
+    map.insert("path", req.uri().path());
+    Template::render("error/404", &map)
+}
+
+use self::handlebars::{Helper, Handlebars, Context, RenderContext, Output, HelperResult, JsonRender};
 use rocket::config::Environment;
-use rocket::fairing::AdHoc;
 
-#[get("/<name>")]
-fn named(name: String) -> String {
-    format!("Hello, {}!", name)
+fn wow_helper(
+    h: &Helper<'_, '_>,
+    _: &Handlebars,
+    _: &Context,
+    _: &mut RenderContext<'_>,
+    out: &mut dyn Output,
+) -> HelperResult {
+    if let Some(param) = h.param(0) {
+        out.write("<b><i>")?;
+        out.write(&param.value().render())?;
+        out.write("</b></i>")?;
+    }
+
+    Ok(())
 }
-#[get("/")]
-fn index() -> String {
-    format!("Hello, Home Page!")
-}
+
 fn main() {
+    let lcores_calc = ((num_cpus::get() as f32) * 0.75) as u16;
+    let lcores = if lcores_calc > 1 { lcores_calc } else { 1 };
     let config = Config::build(Environment::Development)
         .address("127.0.0.1")
-        .port(700)
-        .workers(12)
+        .port(8000)
+        .workers(lcores)
         .unwrap();
     rocket::custom(config)
-        .mount("/", routes![index,named])
+        .mount("/", routes![get::index, get::hello, get::about])
+        .register(catchers![not_found])
+        .attach(Template::custom(|engines| {
+            engines.handlebars.register_helper("wow", Box::new(wow_helper));
+        }))
         .launch();
 }
